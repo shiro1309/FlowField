4940cb1cfba36b034509e0bd7c126a11371f97db
shiro1309 <Nicolay5lie@gmail.com>
2025-09-09 23:11:11 +0200

    Changes to be committed: 	modified:   .gitignore 	new file:   .vscode/c_cpp_properties.json 	new file:   .vscode/settings.json 	new file:   .vscode/tasks.json 	modified:   README.md 	modified:   src/main.c

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# FlowField
+# Raylib C Project
+
+This is a simple Raylib project written in C. It serves as a starting point for building games or graphical applications using the [Raylib](https://www.raylib.com/) library.
+
+---
+
+## Compile
+gcc src/main.c -o main -I/mingw64/include -L/mingw64/lib -lraylib -lopengl32 -lgdi32 -lwinmm
+
+## Run
+./main
+
+project-root/
+│── .vscode
+│   │── tasks.json
+│   └── c_cpp_properties.json
+│── src/
+│   └── main.c      # Entry point of the program
+│── README.md
+└── .gitignore
--- src/main.c
+++ src/main.c
@@ -9,7 +9,6 @@ int getParticlePos(int max);
 struct particle {
     double x; // position x
     double y; // position y
-    Vector2 velocity; // used to get speed and direction with max of 1 and min of -1
     Vector2 trail[20]; // vector trail vill be sett to the position and pased down
 } particle;
 
@@ -22,14 +21,25 @@ int main(void)
     // Initialization
     const int screenWidth  = 1600;
     const int screenHeight = 800;
-    const int particleAmount = 100;
-    const int flowResolution = 100;
+    const int particleAmount = 1000;
+    const int flowResolution = 10;
     const float alpha = 0.05; // rate of change for the vectors
+    const float speed = 1.1;
+
+    float angle = 0.0f;
+    float *anglePtr = &angle;
+
+    int column = 0;
+    int *columnPtr = &column;
+
+    int row = 0;
+    int *rowPtr = &row;
+
 
     float FlowGrid[screenWidth/flowResolution][screenHeight/flowResolution];
     for(int column = 0; column < screenWidth/flowResolution; column++) {
         for(int row = 0; row < screenHeight/flowResolution; row++) {
-            float angle = (row / (float)(screenHeight/flowResolution) * PI);
+            *anglePtr = (row / (float)(screenHeight/flowResolution) * PI);
             FlowGrid[column][row] = angle;
         }
     }
@@ -43,7 +53,13 @@ int main(void)
         Particles[i].y = getParticlePos(screenHeight);
     }
 
-
+    for(int i = 0; i< particleAmount; i++) {
+        *anglePtr = FlowGrid[(int)(Particles[i].x/(screenWidth/flowResolution))][(int)(Particles[i].y/(screenHeight/flowResolution))];
+        int step_x = speed * cos(angle);
+        int step_y = speed * sin(angle);
+        Particles[i].x += step_x;
+        Particles[i].y += step_y;
+    }
     
     //--------------------------------------------------------------------------------------
 
@@ -55,20 +71,63 @@ int main(void)
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
-        // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
+        // update the particle for the new information
+        
+        for(int i = 0; i< particleAmount; i++) {
+            *columnPtr = (int)(Particles[i].x/flowResolution);
+            *rowPtr = (int)(Particles[i].y/flowResolution);
+            
+            if (column > screenWidth/flowResolution-1 || column < 1){
+                continue;
+            }
+            if (row > screenHeight/flowResolution-1 || row < 1) {
+                continue;
+            }
+            
 
+            *anglePtr = FlowGrid[column][row];
+
+            Particles[i].x += speed * cos(angle);
+            Particles[i].y += speed * sin(angle);
+        }
+        
+        /*
+        int column = Particle.x/flowResolution;
+        int row = Particle.y/flowResolution;
+        float angle = FlowGrid[column][row];
+        printf("%f\n", angle);
+        Particle.x += speed * cos(angle);
+        Particle.y += speed * sin(angle);
+        */
+        
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
 
             ClearBackground(RAYWHITE);
-            for(int i = 0; i < particleAmount; i++){
+            
+            
+            
+            for(int column = 0; column < screenWidth/flowResolution; column++) {
+                for(int row = 0; row < screenHeight/flowResolution; row++) {
+                    *anglePtr = FlowGrid[column][row];
+                    int x = column*flowResolution + 10 * cos(angle);
+                    int y = row*flowResolution + 10 * sin(angle);
+                    DrawLine(column*flowResolution, row*flowResolution, column*flowResolution + 10 * cos(angle), row*flowResolution + 10 * sin(angle), BLACK);
+                }
+            }
+            
+            for(int i = 0; i < particleAmount; i++) {
+                
                 DrawCircle(Particles[i].x, Particles[i].y, 3, BLACK);
             }
 
+            DrawRectangle(0, 0, 20, screenHeight, BLACK);
+            DrawRectangle(0, 0 , screenWidth, 20, BLACK);
+            DrawRectangle(0, screenHeight-20, screenWidth, 20, BLACK);
+            DrawRectangle(screenWidth-20, 0, 20, screenHeight, BLACK);
+
+            //DrawCircle(Particle.x, Particle.y, 6, RED);
 
             DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
 
